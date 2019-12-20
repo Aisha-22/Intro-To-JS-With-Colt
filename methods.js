1cc758d51e18700b31d87ff315335cd83b747e8d
Aisha <ash.hlatshwayo22@gmail.com>
2019-12-20 21:22:23 +0200

    My intro to JS with Colt

---
+++ methods.js
@@ -0,0 +1,9 @@
+//alert - something that alerts the user
+alert("I love girls");
+
+//prompt - is used to get input from a user
+prompt("How old are you");
+var youAge = prompt("What is your age?");
+
+//console.log - prints something to print in the console
+console.log("Girls are pretty");
